9f229e57cf26e0ceb1e094ce31a9da626d7ef04f
Data-Structure <2441038792@qq.com>
2022-09-24 21:45:52 +0800

    3

--- Link/Link.cpp
+++ Link/Link.cpp
@@ -1,2 +1,19 @@
 #include "Link.h"
 
+LinkList::LinkList() {
+    this->L = new Node;
+    this->L->next = nullptr;
+    cout << "Creat Successfully" << endl;
+}
+
+LinkList::~LinkList() {
+    cout << "Delete" << endl;
+}
+
+bool LinkList::listInsert(int i, int data) {
+
+}
+
+void LinkList::creatList_R(int data) {
+
+}
--- Link/Link.h
+++ Link/Link.h
@@ -1,2 +1,29 @@
 #include <iostream>
+using namespace std;
 
+struct Book {
+    int id;//编号
+};
+
+struct Node {
+    Book data;//数据域
+    struct Node *next;//指针域
+};
+
+class LinkList {
+public:
+    LinkList();
+    ~LinkList();
+
+    /**
+     * @param i 插入位置
+     * @param data 插入的书的编号
+     */
+    bool listInsert(int i, int data);//数据插入
+
+    void creatList_R(int data);//尾插法
+
+
+private:
+    Node *L;//头节点，地址固定不动
+};
